b8ba896def89f8ab13571970d37c61f4c30bc550
Lucas Muniz <lucaslima1048@gmail.com>
2023-06-06 16:34:36 -0300

    creating project and classes

---
+++ src/exceptions/InvalidCardException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class InvalidCardException extends OnitamaGameException {
+    /**
+     * Construtor que recebe uma mensagem e repassa para a superclasse
+     * @param message A mensagem descrevendo o motivo do problema
+     */
+    public InvalidCardException(String message) {
+        super(message);
+    }
+}
